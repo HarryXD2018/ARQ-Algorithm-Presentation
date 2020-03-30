9468a285ad8d65aecc81cabc22641c14d4278947
付铭 <fuming.137@icloud.com>
2020-03-30 12:27:19 +0800

    update

--- python version/stop_and_wait.py
+++ python version/stop_and_wait.py
@@ -3,11 +3,15 @@ from stop_and_wait_sender import *
 from multiprocessing import Queue
 
 if __name__ == "__main__":
+    queue = Queue()
     data_to_send = "Hello, World"
-    queue = Queue()  # 此队列用于进程通信
-    sender = Sender(data_to_send, queue)
+    sender = Sender(data_to_send,queue)
     receiver = Receiver(queue)
+
     sender.start()
     receiver.start()
     # sender.join()
     # receiver.join()
+
+
+
