d15ebb9def22d99b8c3943fceeb0beca02ae80d2
H.Chen <chenhj2000@163.com>
2020-03-30 10:20:26 +0800

    Unfinished Thread in python version

---
+++ stop_and_wait.py
@@ -0,0 +1,15 @@
+from stop_and_wait_receiver import *
+from stop_and_wait_sender import *
+
+if __name__ == "__main__":
+    data_to_send = "Hello, World"
+    sender = Sender(data_to_send)
+    receiver = Receiver()
+
+    sender.start()
+    receiver.start()
+    # sender.join()
+    # receiver.join()
+
+
+
--- stop_and_wait_sender.py
+++ stop_and_wait_sender.py
@@ -3,17 +3,23 @@
 
 import threading
 import time
+from multiprocessing import Queue
 
 
 class Sender(threading.Thread):
     def __init__(self, content:str):
         threading.Thread.__init__(self) # 父类的init
+        self.queue1 = Queue()     # 创建一个队列，用于进程通信
+        self.queue2 = Queue()
+        self.e = threading.Event()
+        self.lock = threading.Lock()
         self.s = 0  # 发送方控制变量
         self.can_send: bool = True
         self.store_frame = ""
         self.frames = []
         self.frame_init(content)
-        self.ACK = -1    # 用于接收ACK信号
+        self.ACK = 0    # 用于接收ACK信号
+        print("sender inti")
 
     def frame_init(self, content:str):
         # 模拟数据链路层之上的层次
@@ -24,19 +30,22 @@ class Sender(threading.Thread):
         parity = 0
         for ch in self.frames.pop(0):   # 将第一个帧取出（不放回）
             parity += int(ch)
-        parity = parity // 2
+        parity = parity % 2
         frame = "{}{}{}".format(parity, self.frames[0], self.s)
         return frame
 
     def send_frame(self, frame):
         print("Send frame: Parity:{}\t Frame:{}\t sequence_number:{}".format(frame[0], frame[1:-1], frame[-1]))
+        self.queue1.put(frame)
         return frame
 
     def resend_frame(self):
         print("Resend Frame {}".format(self.store_frame))
+        self.queue1.put(self.store_frame)
         return self.store_frame
 
-    def receive(self, frame_ack):
+    def receive(self):
+        frame_ack = self.queue2.get()
         self.ACK = int(frame_ack)
 
     def run(self):
@@ -46,18 +55,23 @@ class Sender(threading.Thread):
                 # 如果没有需要发送的帧，就退出了
                 # 注：这不是现实中的，只是便于程序的运行
                 break
+            self.lock.acquire()
             if self.can_send:
                 frame = self.make_frame()
                 self.store_frame = frame    # 保留副本
                 send_time = time.time()     # 计时器开始计时
-                self.s = (self.s + 1)//2    # s  变量取反
+                self.s = (self.s + 1)%2    # s  变量取反
                 self.send_frame(frame)      # 发送副本
                 self.can_send = False
+                # time.sleep(2)
+            self.lock.release()
+            self.receive()              # 接收返回的ACK帧
+            # self.lock.acquire()
             if self.ACK == self.s:      # 成功接收
                 send_time = 0
                 self.can_send = True
                 self.store_frame = ""   # 清除副本
-            if time.time() - send_time > 1:     # Timeout 1s
+            if time.time() - send_time > 3:     # Timeout 1s
                 send_time = time.time()         # 重新计时
                 self.resend_frame()
 
