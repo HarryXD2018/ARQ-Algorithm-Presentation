f0bf58a42ab37733b15f4545d56a016ae2f72846
付铭 <fuming.137@icloud.com>
2020-03-31 09:57:06 +0800

    update ReceiverService.java and README.md

--- python version/ReceiverService.java
+++ python version/ReceiverService.java
@@ -1,39 +1,58 @@
-package com.springboot.service;
+package com.wxapp.springboot.service;
 
 import org.springframework.stereotype.Service;
 
+import java.io.*;
+import java.util.Date;
+
 @Service
 public class ReceiverService {
-    int r=0;
-    String content="";
-    boolean flag=true;
-    public String handle(String frame) {
-        if(frame.equals("011001011")&&flag) {System.out.println("模拟出错");frame="010001011";flag=false;}
-        String seq_number = frame.substring(frame.length()-1);//序列号
-        int sum_frame = 0;  //对数据位和发送方冗余位求和
-        for(int i=0;i<frame.length()-1;++i)
-            sum_frame += Integer.parseInt(String.valueOf(frame.charAt(i)));
-        int parity = (sum_frame % 2 == 0?1:0);//偶校验结果
-        int seq_match = (this.r ==Integer.parseInt(seq_number)?1:0);  //判断是重发帧还是新的帧
-        if (parity!=1){
-            //未通过偶校验，数据出错
-            System.out.println("未通过偶校验，数据出错");
+    int r = 0;
+    String content = "";
+    boolean flag = true;
+    Long startTs = System.currentTimeMillis();
+
+    public String handle(String frame) throws IOException {
+        if (frame.equals("011001011") && flag) {
+            log("模拟出错");
+            frame = "010001011";
+            flag = false;
         }
-        else if(seq_match==1){
-            String content_b=frame.substring(1,frame.length()-1);
-            content=content+toString(content_b);
-            System.out.println("接收到数据:"+content);
-            this.r = (this.r+1)%2;
-            return this.r+"";
+        String seq_number = frame.substring(frame.length() - 1);//序列号
+        int sum_frame = 0;  //对数据位和发送方冗余位求和
+        for (int i = 0; i < frame.length() - 1; ++i)
+            sum_frame += Integer.parseInt(String.valueOf(frame.charAt(i)));
+        int parity = (sum_frame % 2 == 0 ? 1 : 0);//偶校验结果
+        int seq_match = (this.r == Integer.parseInt(seq_number) ? 1 : 0);  //判断是重发帧还是新的帧
+        if (parity != 1) {
+            //未通过偶校验，数据出错
+            log("未通过偶校验，数据出错");
+        } else if (seq_match == 1) {
+            String content_b = frame.substring(1, frame.length() - 1);
+            if (System.currentTimeMillis() - startTs > 10000) content = "";
+            content = content + toString(content_b);
+            log("接收到数据:" + content);
+            startTs = System.currentTimeMillis();
+            this.r = (this.r + 1) % 2;
+            return this.r + "";
         }
         return "Timeout";
     }
 
+    public void log(String str) throws IOException {
+        Date date = new Date();
+        FileWriter fileWriter;
+        fileWriter = new FileWriter("/root/计网实验.txt", true);
+        fileWriter.write(date.toString() + " " + str + "\r\n");
+        fileWriter.flush();
+        fileWriter.close();
+    }
+
     public static String toString(String binary) {
-        String[] tempStr=binary.split(" ");
-        char[] tempChar=new char[tempStr.length];
-        for(int i=0;i<tempStr.length;i++) {
-            tempChar[i]=BinstrToChar(tempStr[i]);
+        String[] tempStr = binary.split(" ");
+        char[] tempChar = new char[tempStr.length];
+        for (int i = 0; i < tempStr.length; i++) {
+            tempChar[i] = BinstrToChar(tempStr[i]);
         }
         return String.valueOf(tempChar);
     }
@@ -41,22 +60,38 @@ public class ReceiverService {
 
     //将二进制字符串转换成int数组
     public static int[] BinstrToIntArray(String binStr) {
-        char[] temp=binStr.toCharArray();
-        int[] result=new int[temp.length];
-        for(int i=0;i<temp.length;i++) {
-            result[i]=temp[i]-48;
+        char[] temp = binStr.toCharArray();
+        int[] result = new int[temp.length];
+        for (int i = 0; i < temp.length; i++) {
+            result[i] = temp[i] - 48;
         }
         return result;
     }
 
 
     //将二进制转换成字符
-    public static char BinstrToChar(String binStr){
-        int[] temp=BinstrToIntArray(binStr);
-        int sum=0;
-        for(int i=0; i<temp.length;i++){
-            sum +=temp[temp.length-1-i]<<i;
+    public static char BinstrToChar(String binStr) {
+        int[] temp = BinstrToIntArray(binStr);
+        int sum = 0;
+        for (int i = 0; i < temp.length; i++) {
+            sum += temp[temp.length - 1 - i] << i;
         }
-        return (char)sum;
+        return (char) sum;
+    }
+
+    public String testLog() {
+        File file = new File("/root/计网实验.txt");
+        StringBuilder result = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));//构造一个BufferedReader类来读取文件
+            String s;
+            while ((s = br.readLine()) != null) {//使用readLine方法，一次读一行
+                result.append("<p>" + System.lineSeparator() + s + "</p>");
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result.toString();
     }
 }
